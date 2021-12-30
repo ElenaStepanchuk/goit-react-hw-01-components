52a38c6bf42935822a4023cbb7852c27e0d74fdd
ElenaStepanchuk <podolyak365@gmail.com>
2021-12-30 20:53:23 +0200

    task1-styles

--- src/components/task2/Statistics.js
+++ src/components/task2/Statistics.js
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
+import css from '../task2/';
 
 export default function Statistics({title, stats}) {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className="stat-list">
+      <ul className={css.statList}>
         {stats.map((stat) => {
-          return (<li key={stat.id} className="item">
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}%</span>
+          return (<li key={stat.id} className={css.item}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>)
               
         })}
